49b327dd8557f4451d252370cf19f78323f19882
AlwiM26 <alwimhd01@gmail.com>
2024-11-01 01:19:30 +0700

    Add barbecue for winbar component

---
+++ nvim/lua/alwi/plugins/barbecue.lua
@@ -0,0 +1,16 @@
+return {
+  "utilyre/barbecue.nvim",
+  name = "barbecue",
+  version = "*",
+  theme = "auto",
+  dependencies = {
+    "SmiteshP/nvim-navic",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    attach_navic = true,
+    show_navic = true,
+    show_dirname = true,
+    show_basename = true,
+  }
+}
