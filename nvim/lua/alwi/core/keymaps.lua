2e6af2509b418ff457bde6dd1646713e23648d3e
AlwiM26 <alwimhd01@gmail.com>
2024-11-23 03:55:51 +0700

    Add ohmyposh theme config, remove tab management keymap, add window split management keymap

--- nvim/lua/alwi/core/keymaps.lua
+++ nvim/lua/alwi/core/keymaps.lua
@@ -9,18 +9,21 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlight" })
 -- Window management
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>sk", "<C-w>+", { desc = "Increase current window height" })
+keymap.set("n", "<leader>sj", "<C-w>-", { desc = "Decrease current window height" })
+keymap.set("n", "<leader>s>", "<C-w>>", { desc = "Increase current window width" })
+keymap.set("n", "<leader>s<", "<C-w><", { desc = "Decrease current window width" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Resize split window to equal size" })
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close active split window" })
 
 -- Tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer / file in new tab" })
+-- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+-- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+-- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+-- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer / file in new tab" })
 
 --Buffer management
 keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Move to next buffer" })
 keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", { desc = "Move to previous buffer" })
 keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "Remove current file from buffer" })
-
