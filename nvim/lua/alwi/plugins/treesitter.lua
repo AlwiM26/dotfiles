f24ff824f1894c34b57e9c8045f0f782fc9bd588
AlwiM26 <alwimhd01@gmail.com>
2024-11-29 18:44:26 +0700

    change incremental selection using ctrl n

--- nvim/lua/alwi/plugins/treesitter.lua
+++ nvim/lua/alwi/plugins/treesitter.lua
@@ -1,62 +1,61 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" }, -- setup for treesitter to only run when opening a new or exisiting file
-  build = ":TSUpdate",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-  },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" }, -- setup for treesitter to only run when opening a new or exisiting file
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
 
-    treesitter.setup({
-      -- Highlight syntax
-      highlight = {
-        enable = true,
-      },
+		treesitter.setup({
+			-- Highlight syntax
+			highlight = {
+				enable = true,
+			},
 
-      -- Enable indentation
-      indent = {
-        enable = true,
-      },
+			-- Enable indentation
+			indent = {
+				enable = true,
+			},
 
-      -- Enable autotagging
-      autotag = {
-        enable = true,
-      },
+			-- Enable autotagging
+			autotag = {
+				enable = true,
+			},
 
-      -- Ensure these language are installed
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "graphql",
-        "prisma",
-        "bash",
-        "lua",
-        "vim",
-        "gitignore",
-        "vimdoc",
-        "c",
-        "sql",
-        "scss",
-        "regex",
-        "python",
-        "dart",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end
+			-- Ensure these language are installed
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"graphql",
+				"prisma",
+				"bash",
+				"lua",
+				"vim",
+				"gitignore",
+				"vimdoc",
+				"c",
+				"sql",
+				"scss",
+				"regex",
+				"python",
+				"dart",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-n>",
+					node_incremental = "<C-n>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
-
