cbbb341931cf18dbf3e34f83c1241a86b597b7b4
AlwiM26 <AlwiMhd01@gmail.com>
2025-05-10 15:21:41 +0700

    Remove barbecue.nvim, Add description for all command in which-key, change greeter header in alpha and add hidden file in telescope file search

--- nvim/lua/alwi/plugins/which-key.lua
+++ nvim/lua/alwi/plugins/which-key.lua
@@ -1,11 +1,24 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {
-    -- leave it empty to use default settings
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
+	opts = {
+		icons = {
+			mappings = false,
+		},
+		spec = {
+			{ "<leader>b", group = "Buffers" },
+			{ "<leader>e", group = "Files" },
+			{ "<leader>f", group = "Finds" },
+			{ "<leader>h", group = "Git" },
+			{ "<leader>m", group = "Format line" },
+			{ "<leader>n", group = "Remove Highlight" },
+			{ "<leader>s", group = "Split screen" },
+			{ "<leader>w", group = "Sessions" },
+			{ "<leader>x", group = "Diagnostics" },
+		},
+	},
 }
