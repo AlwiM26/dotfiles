b0f99bcb881cba65c4148aa5f85d1b798655574e
AlwiM26 <AlwiMhd01@gmail.com>
2025-05-31 14:52:50 +0700

    Customize lualine looks

--- nvim/lua/alwi/plugins/lualine.lua
+++ nvim/lua/alwi/plugins/lualine.lua
@@ -4,10 +4,26 @@ return {
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status")
+		local default_theme = { fg = "#ebdbb2", bg = "#282828" }
+		local inactive_theme = { fg = "#665c54", bg = "#282828" }
 
 		lualine.setup({
 			options = {
-				theme = "gruvbox-material",
+				-- theme = "gruvbox-material",
+				theme = {
+					normal = {
+						a = { fg = "#f38019", bg = "#282828" },
+						b = default_theme,
+						y = default_theme,
+						z = default_theme,
+					},
+					inactive = {
+						a = { fg = "#f38019", bg = "#282828" },
+						b = inactive_theme,
+						y = inactive_theme,
+						z = default_theme,
+					},
+				},
 				icons_enabled = true,
 				component_separators = "",
 				section_separators = "",
@@ -23,12 +39,15 @@ return {
 				},
 				lualine_b = {
 					{ "buffers" },
+				},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {
 					{ "branch" },
 					{ "diff" },
 					{ "diagnostics" },
 				},
-				lualine_c = {},
-				lualine_x = {
+				lualine_z = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
